1dd87772ecbfa71ee890a03d97159c2349370d5e
chihuong28502 <chihuong28502@gmail.com>
2024-06-12 17:40:49 +0700

    step1

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/comp/header/Header";
+import 'flowbite/dist/flowbite.min.css';
+export default function App() {
+  return (
+    <>
+    <Header />
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    
+    </>
+  )
+}
